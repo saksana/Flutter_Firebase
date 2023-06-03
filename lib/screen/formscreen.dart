993bbda421a6994290e2c732bf840f653c8dd16d
saksana <saksana2015@gmail.com>
2023-06-03 09:22:11 +0700

    wait to save

--- lib/screen/formscreen.dart
+++ lib/screen/formscreen.dart
@@ -17,85 +17,107 @@ class _FormScreenState extends State<FormScreen> {
   Student myStudent = Student();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("ແບບຟອມບັນທຶກຄະແນນ"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: formkey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text("ຊື່", style: TextStyle(fontSize: 18)),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "ກະລຸນາປ້ອນຊື່"),
-                  onSaved: (String? fname) {
-                    myStudent.fname = fname!;
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Text("ນາມສະກຸນ", style: TextStyle(fontSize: 18)),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "ກະລຸນາປ້ອນນາມສະກຸນ"),
-                  onSaved: (String? lname) {
-                    myStudent.lname = lname!;
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Text("ອີເມວ", style: TextStyle(fontSize: 18)),
-                TextFormField(
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: "ກະລຸນາປ້ອນອີເມວ"),
-                    EmailValidator(errorText: "ຮູບແບບອີເມວບໍ່ຖືກຕ້ອງ")
-                  ]),
-                  onSaved: (String? email) {
-                    myStudent.emai = email!;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Text("ຄະແນນ", style: TextStyle(fontSize: 18)),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "ກະລຸນາປ້ອນຄະແນນ"),
-                  onSaved: (String? score) {
-                    myStudent.score = score!;
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      if (formkey.currentState!.validate()) {
-                        formkey.currentState!.save();
-                        //print(myStudent.fname);
-                      }
-                    },
-                    child: const Text("ບັນທຶກຂໍ້ມູນ",
-                        style: TextStyle(fontSize: 18)),
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(title: Text("error")),
+              body: Center(
+                child: Text("${snapshot.error}"),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: const Text("ແບບຟອມບັນທຶກຄະແນນ"),
+              ),
+              body: Container(
+                padding: const EdgeInsets.all(20),
+                child: Form(
+                  key: formkey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text("ຊື່", style: TextStyle(fontSize: 18)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: "ກະລຸນາປ້ອນຊື່"),
+                          onSaved: (String? fname) {
+                            myStudent.fname = fname!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text("ນາມສະກຸນ", style: TextStyle(fontSize: 18)),
+                        TextFormField(
+                          validator: RequiredValidator(
+                              errorText: "ກະລຸນາປ້ອນນາມສະກຸນ"),
+                          onSaved: (String? lname) {
+                            myStudent.lname = lname!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text("ອີເມວ", style: TextStyle(fontSize: 18)),
+                        TextFormField(
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "ກະລຸນາປ້ອນອີເມວ"),
+                            EmailValidator(errorText: "ຮູບແບບອີເມວບໍ່ຖືກຕ້ອງ")
+                          ]),
+                          onSaved: (String? email) {
+                            myStudent.emai = email!;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text("ຄະແນນ", style: TextStyle(fontSize: 18)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: "ກະລຸນາປ້ອນຄະແນນ"),
+                          onSaved: (String? score) {
+                            myStudent.score = score!;
+                          },
+                          keyboardType: TextInputType.number,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              if (formkey.currentState!.validate()) {
+                                formkey.currentState!.save();
+                                //print(myStudent.fname);
+                              }
+                            },
+                            child: const Text("ບັນທຶກຂໍ້ມູນ",
+                                style: TextStyle(fontSize: 18)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
